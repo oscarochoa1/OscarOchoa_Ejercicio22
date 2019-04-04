a1d6573c2cc52b0e1016dd680060bc9b3b3b441a
oscarochoa1 <46940987+oscarochoa1@users.noreply.github.com>
2019-04-04 16:03:28 -0500

    Update OscarOchoa_Ejercicio22.cpp

--- OscarOchoa_Ejercicio22.cpp
+++ OscarOchoa_Ejercicio22.cpp
@@ -10,7 +10,7 @@ int i;
 float ii;
 
 int main(){
-    drand(48)
+    drand(48);
     numero = rand();
     for (i=0;i=1000; i++){
         funcion(1000);
@@ -31,10 +31,11 @@ void funcion(int N){
     float y (float x){
         float x = exp(-x*x/2);
         if ( (x > 3.14) | (x<0)){
-            y = 0
+            y = 0;
         }else {
-        ii = ( x > 3.14) | ( x < 0)
-        ii = 0}
+        ii = ( x > 3.14) | ( x < 0);
+        ii = 0;
+        }
     }
 }       
 
