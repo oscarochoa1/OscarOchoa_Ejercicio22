fdbcf3f10f70a6243ca2999fb8681e8aafa25133
oscarochoa1 <46940987+oscarochoa1@users.noreply.github.com>
2019-04-04 16:05:41 -0500

    Update Ejercicio22.py

--- Ejercicio22.py
+++ Ejercicio22.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-a = os.system("g++ OscarOchoa_Ejercicio.cpp -o OscarOchoa_Ejercicio.x")
-a = os.system("./OscarOchoa_Ejercicio.x > OscarOchoa_Ejercicio.dat")
+a = os.system("g++ OscarOchoa_Ejercicio22.cpp -o OscarOchoa_Ejercicio22.x")
+a = os.system("./OscarOchoa_Ejercicio22.x > OscarOchoa_Ejercicio22.dat")
 
 plt.figure()
 data = np.loadtxt("OscarOchoa_Ejercicio.dat")
